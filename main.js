e549eff48635a466e1bb28e6233d4619585c18b2
Gina González <gini_go@hotmail.com>
2024-07-08 11:27:45 +0200

    add html, css and javascript files

---
+++ main.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
+
+const list = document.querySelector(".js-list");
+
+const rendertasks = () => {
+  for (const task of tasks) {
+    console.log(task);
+    list.innerHTML += `<li> ${task.name}</li>`;
+  }
+};
+rendertasks();

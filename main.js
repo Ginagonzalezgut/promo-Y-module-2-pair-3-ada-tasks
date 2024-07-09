0469bf27c1d43fb53ff1e675eb43c1268db99cd3
Gina González <gini_go@hotmail.com>
2024-07-09 14:37:41 +0200

    add checkbox toggle

--- main.js
+++ main.js
@@ -12,27 +12,33 @@ const tasks = [
 ];
 
 const list = document.querySelector(".js-list");
-const secondButton = document.querySelector('.Secondbutton')
-
+const secondButton = document.querySelector(".Secondbutton");
 
 const rendertasks = () => {
+  list.innerHTML = "";
+
   for (const task of tasks) {
     if (task.completed === true) {
-      list.innerHTML += `<li class="checked"><input checked type="checkbox" id="${task.id}" name="" />${task.name}
+      list.innerHTML += `<li class="checked"><input class="js-checkbox" checked type="checkbox" id="${task.id}" name="" />${task.name}
       </li>`;
     } else {
-      list.innerHTML += `<li><input type="checkbox" id="${task.id}"  name="" />${task.name}
+      list.innerHTML += `<li><input class="js-checkbox" type="checkbox" id="${task.id}"  name="" />${task.name}
       </li>`;
     }
   }
+
+  const checkboxes = document.querySelectorAll(".js-checkbox");
+
+  for (const checkbox of checkboxes) {
+    checkbox.addEventListener("click", () => {
+      const checboxId = parseInt(checkbox.id);
+      const taskToCheck = tasks.find((task) => {
+        return task.id === checboxId;
+      });
+      taskToCheck.completed = !taskToCheck.completed;
+      rendertasks();
+    });
+  }
 };
-
-
-function handleClick(){
-  rendertasks.classList.remove(checkbox)
-}
-
-secondButton.addEventListener('click', handleClick)
-
 
 rendertasks();

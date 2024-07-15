aebf3204ae4827c626afb3beceaf0683ffd90214
Gina González <gini_go@hotmail.com>
2024-07-15 10:23:04 +0200

    add new changes

--- dom.js
+++ dom.js
@@ -1,10 +1,11 @@
-const list = document.querySelector(".js-list2");
+const list2 = document.querySelector(".js-list2");
 
 const liElement = document.createElement("li");
 liElement.setAttribute("class", "checked");
-list.appendChild(liElement);
+list2.appendChild(liElement);
 console.log(list);
 const input = document.createElement("input");
 liElement.appendChild(input);
 input.setAttribute("class", "js-checkbox");
+input.setAttribute("type", "checkbox");
 console.log(list);
